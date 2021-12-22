db7e1aeaa850e16ff9cd33a8ad43030beff0cf39
valadzkoaliaksei <valadzkoaliaksei@tut.by>
2021-12-22 14:50:28 +0300

    feat: Старт проекта

---
+++ src/features/counter/counterAPI.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-promise-executor-return */
+// A mock function to mimic making an async request for data
+export const fetchCount = (amount = 1) =>
+  new Promise<{ data: number }>((resolve) => setTimeout(() => resolve({ data: amount }), 500));
